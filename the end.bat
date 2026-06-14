0758483dfa0dd37bc2fd97e45bb09b9c7ea313e9
holdenv9012user <holden.villmer@gmail.com>
2026-06-14 19:10:19 -0400

    Add files via upload

---
+++ the end.bat
@@ -0,0 +1,29 @@
+@echo off
+color 02
+echo Oh hello..
+pause
+cls
+echo I see you found me
+pause
+cls
+echo But you weren't supposed to find me.
+pause
+cls
+echo I will now reveal my secret.
+pause
+cls
+echo My secret is that I am a program that generates random numbers.
+pause
+cls
+echo I will now destroy everything.
+pause
+cls
+echo Destroying everything...
+pause
+cls
+echo This is the end.
+pause
+cls
+:Start
+echo  %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto Start
